18f14438d9b6eabf059d30489fe740b469727d72
Anastasia <115793250+Kreslavskaia@users.noreply.github.com>
2024-02-20 15:25:27 +0000

    Update Graph_class.ino

--- Graph_class.ino
+++ Graph_class.ino
@@ -5,8 +5,9 @@ class Graph {
     int number_of_connections;
     int array_graphs[number_of_connections];
     int array_directions[numver_of_connections]; //0 - unassigned, 1 - North, 2 - East, 3 - South, 4 - West
+    //"left" (3), "forward" (0), "right" (1), "backwards (2)", equals to direction 2 - direction 1
     long array_weights[number_of_connections];
-    // unsigned long lastDebounceTime = 0; Google what unsigned 
+    // unsigned long lastDebounceTime = 0; Google what unsigned is
 
   public:
 
@@ -33,31 +34,55 @@ class Graph {
       array_weights = weights_given;
     }
 
-    int get_compass(int final_destination) {
-      // This function just gives the idea to which side of the compass it needs to go
+    int number_from_compas(int current_compas) { // shows origin location from current compas at current graph
       // 0 - unassigned, 1 - North, 2 - East, 3 - South, 4 - West
-      found_compass_direction = 0;
-      for(int i = 0; i< number_of_connections; i++){
-        if (array_graphs[i] == final_destination) {
-          found_array_location = array_directions[i];
+      found_number = 4; // having 4 at the end means you have done something wrong
+      // If current compass is 2 (East), it is the opposite of going to 2 + 2 = 4 (West), and we are looking to location of point with origin 1 + (n+1)%4
+      search_value = 1 + (current_compas + 1) % 4
+      for(int i = 0; i < number_of_connections; i++){
+        if (array_directions[i] == search_value) {
+          found_number = i;
         }
       }
-      /**if (found_compass_direction == 0) {
-        // Write code for warning
-      }**/
-      return(found_compass_direction);
+      return(found_number);
     }
 
-    
-    
-    int get_direction(int final_destination, int current_direction) { //Returns "left" (0), "forward" (1), "right" (2), "backwards (3)"
-      // Current direction is b
-      update();
-      return state;
+    int number_from_destination(int final_destination) {
+      x = 100; // if it does not change, something is wrong 
+      for(int i = 0; i < number_of_connections; i++){
+        if (array_graphs[i] == final_destination) {
+          x = i;
+        }
+      }
+      return x;
     }
 
-    bool isPressed() {
-      return (getState() == HIGH);
+    int get_mode_of_motion(int final_destination, int current_compas, bool back){ //Returns "left" (3), "forward" (0), "right" (1), "backwards (2)", equals to direction 2 - direction 1
+      x = 100;
+      if (number_of_connections == 1) {
+        x = 8; // go back
+      } else if (number_of_connections == 2) {
+        x = 1; // go straight
+      } else {
+
+        origin = number_from_compas(current_compas);
+        destination = number_from_destination(final_destination);
+        
+        if (back) { //car was moving backwards
+          if (origin == 0 && destination == 2) {x = 9}
+          if (origin == 0 && destination == 1) {x = 10}
+          if (origin == 1 && destination == 0) {x = 11}
+          if (origin == 2 && destination == 0) {x = 12}
+        } else { //car was moving normally
+          if (origin == 1 && destination == 2) {x = 2}
+          if (origin == 2 && destination == 1) {x = 3}
+          if (origin == 0 && destination == 1) {x = 4}
+          if (origin == 0 && destination == 2) {x = 5}
+          if (origin == 1 && destination == 0) {x = 6}
+          if (origin == 2 && destination == 0) {x = 7}
+        }
+      }
+      return x;
     }
 
 };
