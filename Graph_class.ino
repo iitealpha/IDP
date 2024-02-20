d2fa08bd49d331fe9f0c5d9d7c8e676d431b4c65
Anastasia <115793250+Kreslavskaia@users.noreply.github.com>
2024-02-20 16:24:46 +0000

    Update Graph_class.ino

--- Graph_class.ino
+++ Graph_class.ino
@@ -5,30 +5,35 @@ class Graph {
     int number_of_connections;
     int array_graphs[3];
     int array_directions[3]; //0 - unassigned, 1 - North, 2 - East, 3 - South, 4 - West
-    //"left" (3), "forward" (0), "right" (1), "backwards (2)", equals to direction 2 - direction 1
     // long array_weights[number_of_connections];
     // unsigned long lastDebounceTime = 0; Google what unsigned is
+    int array_weights[3];
 
   public:
 
     Graph(int name_given, int number_of_connections_given, int connected_graphs_given[], int corresponding_directions_given[]) {
       this->int_name = name_given;
       this->number_of_connections = number_of_connections_given;      
-      this->array_graphs = connected_graphs_given;
-      this->array_directions = corresponding_directions_given;
+      for (int i = 0; i < 3; ++i) {
+        this->array_graphs[i] = connected_graphs_given[i];
+        this->array_directions[i] = corresponding_directions_given[i];
+      }
     }
 
     void assign_weights(int weights_given[]) {
       // We assign this separately bc this is optional, BUT DO THIS VOID IF YOU WANT AUTOMATIC PATHS FINDER
-      this->array_weights = weights_given;
+      for (int i = 0; i < 3; ++i) {
+        this->array_weights[i] = weights_given[i];
+      }
+      
     }
 
     int number_from_compas(int current_compas) { // shows origin location from current compas at current graph
       // 0 - unassigned, 1 - North, 2 - East, 3 - South, 4 - West
       int found_number = 4; // having 4 at the end means you have done something wrong
       // If current compass is 2 (East), it is the opposite of going to 2 + 2 = 4 (West), and we are looking to location of point with origin 1 + (n+1)%4
-      int search_value = 1 + (current_compas + 1) % 4
-      for(int i = 0; i < number_of_connections; i++){
+      int search_value = 1 + (current_compas + 1) % 4;
+      for (int i = 0; i < 3; i++){
         if (this->array_directions[i] == search_value) {
           found_number = i;
         }
@@ -46,7 +51,7 @@ class Graph {
       return x;
     }
 
-    int get_mode_of_motion(int final_destination, int current_compas, bool back){ //Returns "left" (3), "forward" (0), "right" (1), "backwards (2)", equals to direction 2 - direction 1
+    int get_mode_of_motion(int final_destination, int current_compas, bool back){
       int x = 100; // if it does not change, something is wrong 
       if (number_of_connections == 1) {
         x = 8; // go back
