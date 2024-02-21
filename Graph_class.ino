d4152da9e0d2b574edf0df691c55a72abdd07124
ak2459 <ak2459@cam.ac.uk>
2024-02-21 10:05:48 +0000

    commit

--- Graph_class.ino
+++ Graph_class.ino
@@ -1,6 +1,8 @@
 class Graph {
   
   private:
+    static int current_compass; // This is the static variable that represents current motion of the 
+    static bool backwards; // Becomes true when moving backwards
     int int_name;
     int number_of_connections;
     int array_graphs[3];
@@ -12,6 +14,8 @@ class Graph {
   public:
 
     Graph(int name_given, int number_of_connections_given, int connected_graphs_given[], int corresponding_directions_given[]) {
+      this->current_compass = 1; // Because it starts from going to the North
+      this->backwards = false; //because it starts moving forward
       this->int_name = name_given;
       this->number_of_connections = number_of_connections_given;      
       for (int i = 0; i < 3; ++i) {
@@ -51,22 +55,24 @@ class Graph {
       return x;
     }
 
-    int get_mode_of_motion(int final_destination, int current_compas, bool back){
+    int get_mode_of_motion(int final_destination){
       int x = 100; // if it does not change, something is wrong 
       if (number_of_connections == 1) {
         x = 8; // go back
+        this->backwards = true; // Because the way it was moving changed
       } else if (number_of_connections == 2) {
         x = 1; // go straight
       } else {
 
-        int origin = number_from_compas(current_compas);
+        int origin = number_from_compas(this->current_compass);
         int destination = number_from_destination(final_destination);
         
-        if (back) { //car was moving backwards
+        if (this->backwards == true) { //car was moving backwards
           if (origin == 0 && destination == 2) {x = 9;}
           if (origin == 0 && destination == 1) {x = 10;}
           if (origin == 1 && destination == 0) {x = 11;}
           if (origin == 2 && destination == 0) {x = 12;}
+          this->backwards = false; // Because the way it was moving changed
         } else { //car was moving normally
           if (origin == 1 && destination == 2) {x = 2;}
           if (origin == 2 && destination == 1) {x = 3;}
