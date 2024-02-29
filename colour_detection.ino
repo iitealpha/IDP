0e753912fc9d33a99d01e7c968db3918afb54ffa
wh365 <115560920+wh365@users.noreply.github.com>
2024-02-29 11:38:22 +0000

    Another_test colour_detection.ino

--- colour_detection.ino
+++ colour_detection.ino
@@ -8,6 +8,56 @@
  * @date        2022-03-16
  * @url         https://github.com/DFRobot/DFRobot_TCS34725
  */
+
+/*!
+ * @file  colorview.ino
+ * @brief Gets the ambient light color
+ * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @license     The MIT License (MIT)
+ * @author      PengKaixing(kaixing.peng@dfrobot.com)
+ * @version     V1.0.0
+ * @date        2022-03-16
+ * @url         https://github.com/DFRobot/DFRobot_TCS34725
+ */
+#include "DFRobot_TCS34725.h"
+
+DFRobot_TCS34725 tcs = DFRobot_TCS34725(&Wire, TCS34725_ADDRESS,TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+void setup() 
+{
+    delay(3000);
+    Serial.begin(115200);
+    Serial.println("Color View Test!");
+}
+
+void loop() {
+  Serial.println("1");
+  uint16_t clear, red, green, blue; // Create variables to store the color values
+  Serial.println("11");
+  tcs.getRGBC(&red, &green, &blue, &clear); // Read the color values and store them in the variables
+  tcs.lock();
+  Serial.println("2");
+    // turn off LED 
+  Serial.print("C:\t"); Serial.print(clear);
+  Serial.print("\tR:\t"); Serial.print(red);
+  Serial.print("\tG:\t"); Serial.print(green);
+  Serial.print("\tB:\t"); Serial.print(blue);
+  Serial.println("\t");
+  Serial.println("3");
+
+    // Figure out some basic hex code for visualization
+  uint32_t sum = clear;
+  float r, g, b;
+  r = red; r /= sum;
+  g = green; g /= sum;
+  b = blue; b /= sum;
+  r *= 256; g *= 256; b *= 256;
+  Serial.print("\t");
+  Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
+  Serial.println((int)r, HEX); 
+  Serial.println("4");
+}
+
+/*
 #include "DFRobot_TCS34725.h"
 
 DFRobot_TCS34725 tcs = DFRobot_TCS34725(&Wire, TCS34725_ADDRESS,TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
@@ -45,3 +95,4 @@ void loop() {
     Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
     Serial.println();
 }
+*/
