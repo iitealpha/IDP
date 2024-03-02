d6b473fd06898898e777eb7902b28f6deb0a86d3
kelhamhillier <123702081+kelhamhillier@users.noreply.github.com>
2024-03-02 22:33:16 +0000

    Create djikstra.ino

---
+++ djikstra.ino
@@ -0,0 +1,81 @@
+CODE DOES NOT WORK, DONT BOTHER RUNNING
+
+//int random_path[] = {0,2,2,5,6,1};
+//int current_graph = 10; //Current graph value
+//int goal_graph = 2; //Goal graph value
+
+//void path_addition(current_graph, y){
+  //Adds graph points to path
+  //int random_path[] = new int[7];
+  //int distances_from_start[] = new int[20];
+  //distances_from_start[] = map_of_sizes[current_graph-1][];
+  
+#include<iostream>
+#include<climits>     
+using namespace std;
+
+// this method returns a minimum distance for the 
+// vertex which is not included in Tset.
+int minimumDist(int dist[], bool Tset[]) 
+{
+	int min=INT_MAX,index;
+              
+	for(int i=0;i<6;i++) 
+	{
+		if(Tset[i]==false && dist[i]<=min)      
+		{
+			min=dist[i];
+			index=i;
+		}
+	}
+	return index;
+}
+
+void Dijkstra(int graph[6][6],int src) // adjacency matrix used is 6x6
+{
+	int dist[6]; // integer array to calculate minimum distance for each node.                            
+	bool Tset[6];// boolean array to mark visted/unvisted for each node.
+	
+	// set the nodes with infinity distance
+	// except for the initial node and mark
+	// them unvisited.  
+	for(int i = 0; i<6; i++)
+	{
+		dist[i] = INT_MAX;
+		Tset[i] = false;	
+	}
+	
+	dist[src] = 0;   // Source vertex distance is set to zero.             
+	
+	for(int i = 0; i<6; i++)                           
+	{
+		int m=minimumDist(dist,Tset); // vertex not yet included.
+		Tset[m]=true;// m with minimum distance included in Tset.
+		for(int i = 0; i<6; i++)                  
+		{
+			// Updating the minimum distance for the particular node.
+			if(!Tset[i] && graph[m][i] && dist[m]!=INT_MAX && dist[m]+graph[m][i]<dist[i])
+				dist[i]=dist[m]+graph[m][i];
+		}
+	}
+	cout<<"Vertex\t\tDistance from source"<<endl;
+	for(int i = 0; i<6; i++)                      
+	{ //Printing
+		char str=65+i; // Ascii values for pritning A,B,C..
+		cout<<str<<"\t\t\t"<<dist[i]<<endl;
+	}
+}
+
+int main()
+{
+	int graph[6][6]={
+		{0, 10, 20, 0, 0, 0},
+		{10, 0, 0, 50, 10, 0},
+		{20, 0, 0, 20, 33, 0},
+		{0, 50, 20, 0, 20, 2},
+		{0, 10, 33, 20, 0, 1},
+		{0, 0, 0, 2, 1, 0}};
+	Dijkstra(graph,0);
+	return 0;	                        
+}
+  
