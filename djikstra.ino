c45232485b989c40bc50e143ac9accb08a49eb92
kelhamhillier <123702081+kelhamhillier@users.noreply.github.com>
2024-03-04 17:00:52 +0000

    Update djikstra.ino

--- djikstra.ino
+++ djikstra.ino
@@ -4,27 +4,70 @@ int random_path[] = {0,2,2,5,6,1}; // An old path
 int current_graph = 10; //Current graph value
 int goal_graph = 2; //Goal graph value
 
-int[] new_path(current_graph, goal_graph){
-	if current_graph==1:{
-		if goal_graph==4:{
-			return {1,8,9,4,4};
-				}
-		else if goal_graph==5:{
-			return {1,8,12,13,14,5,5}
-				}}}
-		else if goal_graph==
-			
+int new_path(current_graph, goal_graph){
 	//Hard coding paths between nodes
+	if (current_graph==1){
+		if (goal_graph==4){
+			return {1,8,9,4,4};
+		}else if (goal_graph==5){
+			return {1,8,12,13,14,5,5};
+		}else if (goal_graph==6){
+			return {1,8,12,13,18,6,6};
+		}else if (goal_graph==7){
+			return {1,8,12,19,16,17,7,7};
+		}else{
+			return {0,0,0};
+		}}
+	if (current_graph==3){
+		if (goal_graph==4){
+			return {3,11,10,9,4,4};
+		}else if (goal_graph==5){
+			return {3,11,15,14,5,5};
+		}else if (goal_graph==6){
+			return {3,11,15,14,13,18,6,6};
+		}else if (goal_graph==7){
+			return {3,11,15,20,17,7,7};
+		}else{
+			return {0,0,0};
+		}}
+	if (current_graph==4){
+		if (goal_graph==1){
+			return {4,9,8,1,1};
+		}else if (goal_graph==3{
+			return {4,9,10,11,3,3};
+		}else{
+			return {0,0,0};
+		}}
+	if (current_graph==5){
+		if (goal_graph==1){
+			return {5,14,13,12,8,1,1};
+		}else if (goal_graph==3){
+			return {5,14,15,11,3,3};
+		}else{
+			return {0,0,0};
+		}}
+	if (current_graph==6){
+		if (goal_graph==1){
+			return {6,18,13,12,8,1,1};
+		}else if (goal_graph==3){
+			return {6,18,13,14,15,11,3,3};
+		}else{
+			return {0,0,0};
+		}}
+	if (current_graph==7){
+		if (goal_graph==1){
+			return {7,17,16,19,12,8,1,1};
+		}else if (goal_graph==3){
+			return {7,17,20,15,11,3,3};
+		}else{
+			return {0,0,0};
+		}}
+	
+	
 	
 }
-
-void path_addition(current_graph, y){
-  //Adds graph points to path
-  int random_path[] = new int[7];
-  int distances_from_start[] = new int[20];
-  distances_from_start[] = map_of_sizes[current_graph-1][];
-}
-
+void loop{
+	random_path=new_path(7,3);}
 
 
 
