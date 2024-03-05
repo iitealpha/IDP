2d1fe8f3224f50fc5f5663e40c92fc7b9298fdf2
timtan <116691346+iitealpha@users.noreply.github.com>
2024-03-05 12:04:37 +0000

    colour_detection code uplaoded

--- Mechanism/Colour_Detection2.ino
+++ Mechanism/Colour_Detection2.ino
@@ -27,18 +27,54 @@ while (1);
 // Now we're ready to get readings!
 }
 
-void loop(void) {
-uint16_t r, g, b, c, colorTemp, lux;
+void color_detection() {
+    uint16_t colorTemp;
+    // uint16_t r, g, b, c, lux;
 
-tcs.getRawData(&r, &g, &b, &c);
-colorTemp = tcs.calculateColorTemperature(r, g, b);
-lux = tcs.calculateLux(r, g, b);
+    tcs.getRawData(&r, &g, &b, &c);
+    colorTemp = tcs.calculateColorTemperature(r, g, b);
+    // lux = tcs.calculateLux(r, g, b);
 
-Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-Serial.println(" ");
+
+    if (colorTemp > 100000) {
+        // turn on red LED for 3 seconds
+        Serial.println("Red");
+        digitalWrite(LED_Red, HIGH);
+        delay(3000);
+        cube_color = 0
+    }
+    else if (colorTemp > 10000) {
+        // turn on green LED for 3 seconds
+        Serial.println("Black");
+        digitalWrite(LED_Green, HIGH);
+        delay(3000);
+        cube_color = 1
+    }
+    else if (colorTemp > 1000) {
+        // turn on blue LED for 3 seconds
+        Serial.println("Nothing");
+        digitalWrite(LED_Blue, HIGH);
+        delay(3000);
+        cube_color = 2
+    }
+    else {
+        Serial.println("Error");
+    }
 }
+// Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+// Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+// Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+// Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+// Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+// Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+// Serial.println(" ");
+// }
+
+
+// const uint8_t LED_Red = 7;
+// const uint8_t LED_Green = 8;
+// const uint8_t LED_Blue = 10;
+
+
+
+
